14efbb3fac75d8e5612b332ab4e27a094f2d8d14
Saksham Kamboj <saksham@ekvayu.com>
2025-07-18 11:10:29 +0530

    add base temp

---
+++ petpals-sanity/sanity.config.js
@@ -0,0 +1,18 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemaTypes'
+
+export default defineConfig({
+  name: 'default',
+  title: 'petpals-sanity',
+
+  projectId: '10vzeis5',
+  dataset: 'production',
+
+  plugins: [structureTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
