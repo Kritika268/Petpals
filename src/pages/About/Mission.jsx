d50b3b71fa377ab8ecfd6d0f95924eb116ccec19
Kritika <kritikadevgan2005@gmail.com>
2025-07-18 19:33:34 +0530

    Initial layout for Mission page

--- src/pages/About/Mission.jsx
+++ src/pages/About/Mission.jsx
@@ -1,5 +1,15 @@
+import { section } from "framer-motion/client";
+import React from "react";
+
 const Mission = () => {
-
-}
-
+  return (
+    <section className="bg-gray-100 py-14 px-4 sm:px-8 lg:px-16">
+      <div className="max-w-5xl mx-auto text-center">
+        <h2 className="text-3xl sm:text-4xl font-bold text-gray-800 mb-6">
+          Our Mission
+        </h2>
+      </div>
+    </section>
+  );
+};
 export default Mission;
