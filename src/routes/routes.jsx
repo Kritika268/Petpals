96712be9f3059805000abae695087dcc34420728
Saksham Kamboj <sakshamkamboj2072@gmail.com>
2025-07-20 12:12:07 +0530

    feat: Refactor App component to use Layout and AppRoutes
    The App component was refactored to improve code organization and maintainability. The previous implementation included all routes and conditional rendering of the Navbar and Footer within the App component.
    
    This commit introduces the following changes:
    
    - Replaced the AppContent component with a Layout component. The Layout component will handle the rendering of the Navbar, Footer, and any other layout-related elements.
    - Created a new AppRoutes component to define all the routes for the application. This component is rendered within the Layout component.
    - Updated the App component to render the Layout component, which in turn renders the AppRoutes component.
    
    These changes improve the separation of concerns and make it easier to maintain and update the application's routing and layout.

---
+++ src/routes/routes.jsx
@@ -0,0 +1,51 @@
+import { Routes, Route } from "react-router-dom";
+
+// Pages
+import Home from "../pages/Home";
+import Telehealth from "../pages/Telehealth";
+import Referral from "../pages/Referral";
+import Plans from "../pages/Plans";
+import Register from "../pages/Register";
+import Appointment from "../pages/Appointment";
+import Service from "../pages/Service";
+import Error from "../pages/Error";
+
+// About Pages
+import Mission from "../pages/About/Mission";
+import Team from "../pages/About/Team";
+import Blog from "../pages/About/Blog";
+import Contact from "../pages/About/Contact";
+
+// Services Pages
+import Packages from "../pages/Services/Packages";
+import Recommendations from "../pages/Services/Recommendations";
+
+function AppRoutes() {
+  return (
+    <Routes>
+      {/* Main Pages */}
+      <Route path="/" element={<Home />} />
+      <Route path="/telehealth" element={<Telehealth />} />
+      <Route path="/referral" element={<Referral />} />
+      <Route path="/plans" element={<Plans />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/appointment" element={<Appointment />} />
+      <Route path="/service" element={<Service />} />
+
+      {/* About Pages */}
+      <Route path="/about/team" element={<Team />} />
+      <Route path="/about/mission" element={<Mission />} />
+      <Route path="/about/blog" element={<Blog />} />
+      <Route path="/about/contact" element={<Contact />} />
+
+      {/* Services Pages */}
+      <Route path="/services/packages" element={<Packages />} />
+      <Route path="/services/recommendations" element={<Recommendations />} />
+
+      {/* Catch-all for 404 */}
+      <Route path="*" element={<Error />} />
+    </Routes>
+  );
+}
+
+export default AppRoutes;
